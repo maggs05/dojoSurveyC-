69efca9672c7134d895670a6bf8104eaf664b8df
maggs05 <maggs.gose05@hotmail.com>
2017-11-14 12:31:28 -0800

    basics laid out

---
+++ Controllers/SurveyController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+namespace dojoSurvey.Controllers{
+    public class SurveyController: Controller{
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index(){
+            ViewBag.Errors=new List<string>();
+            return View();
+        }
+        [HttpPost]
+        [Route("process")]
+        public IActionResult Process(string yourName, string location, string language, string comment){
+            ViewBag.Errors = new List<string>();
+            if(yourName == null){
+                ViewBag.Errors.Add("Name cannot be empty!");
+            }
+            if(location == null){
+                ViewBag.Errors.add("You must select a location!");
+            }
+            if(language == null){
+                ViewBag.Errors.add("Please select a location!");
+            }
+            if(ViewBag.Error.Count>0){
+                return View("Index");
+            }
+            ViewBag.yourName=yourName;
+            ViewBag.location=location;
+            ViewBag.language=language;
+            ViewBag.comment=comment;
+
+            return View("Success");
+        }
+    }
+}
